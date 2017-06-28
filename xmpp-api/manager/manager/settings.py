d9e8d71a215832602e2800cf40115a168d6314bf
Javier S.A <jusafing@gmail.com>
2017-06-28 08:01:31 +0000

    disable debug mode

--- xmpp-api/manager/manager/settings.py
+++ xmpp-api/manager/manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3s#pfb2pl-&kofpzem4!q+9bhkx9w(j+swr$hhn_dql50z!xac'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
